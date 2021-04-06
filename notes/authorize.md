0570fcc0938f4594339d28ade8d47544030d229a
Denys Vitali <denys@denv.it>
2021-04-06 18:22:58 +0200

    feat: get and save Teams + Skype Token

---
+++ notes/authorize.md
@@ -0,0 +1,29 @@
+## Teams Token
+
+```
+response_type: id_token
+client_id: 5e3ce6c0-2b1f-4285-8d4b-75ee78787346
+redirect_uri: https://teams.microsoft.com/go
+state: b14aae3b-db37-44d8-9ac8-9934ace39383
+(empty)
+client-request-id: 3a793562-788a-40ef-a37a-fe419c243e5e
+x-client-SKU: Js
+x-client-Ver: 1.0.9
+nonce: d04e4bd7-aafd-4e76-8e0b-039709856b6b
+```
+
+```
+response_type: token
+client_id: 5e3ce6c0-2b1f-4285-8d4b-75ee78787346
+resource: https://api.spaces.skype.com
+redirect_uri: https://teams.microsoft.com/go
+state: 2cbefcf2-20e4-4f48-b241-c1ca5a91cbdf|https://api.spaces.skype.com
+(empty)
+client-request-id: 53ffe21b-319c-4e65-bdfd-cda38d5bc275
+x-client-SKU: Js
+x-client-Ver: 1.0.9
+prompt: none
+login_hint: John.Doe@contoso.com
+domain_hint: contoso.com
+nonce: 53ffe21b-319c-4e65-bdfd-cda38d5bc275
+```
--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,67 @@ import {
   BrowserWindow, app,
 } from 'electron';
 
-const uuid = require('uuid');
+import { homedir } from 'os';
+import { writeFileSync, existsSync, mkdirSync } from 'fs';
+import { v4 as uuidv4 } from 'uuid';
+import * as jwt from 'jsonwebtoken';
 
+const configPath = '.config/fossteams';
+const DEBUG = false;
+const TEAMS_APP_ID = '5e3ce6c0-2b1f-4285-8d4b-75ee78787346';
+const SKYPE_RESOURCE = 'https://api.spaces.skype.com';
 const USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) MicrosoftTeams-Preview/1.4.00.7556 Chrome/80.0.3987.163 Electron/8.5.5 Safari/537.36';
 
-function getLoginURL() : string {
+type TeamsSkype = 'teams' | 'skype';
+let win : BrowserWindow | null = null;
+let tokenResponseCount = 0;
+
+function getLoginURL(type: TeamsSkype) : string {
   const loginUrl = new URL('https://login.microsoftonline.com');
   loginUrl.pathname = '/common/oauth2/authorize';
-  loginUrl.searchParams.append('response_type', 'id_token');
-  loginUrl.searchParams.append('client_id', '5e3ce6c0-2b1f-4285-8d4b-75ee78787346');
+
+  switch (type) {
+    case 'teams':
+      loginUrl.searchParams.append('response_type', 'id_token');
+      loginUrl.searchParams.append('state', uuidv4());
+      break;
+    case 'skype':
+      loginUrl.searchParams.append('response_type', 'token');
+      loginUrl.searchParams.append('state', `${uuidv4()}|${SKYPE_RESOURCE}`);
+      loginUrl.searchParams.append('resource', SKYPE_RESOURCE);
+      break;
+
+    default:
+      break;
+  }
+  loginUrl.searchParams.append('client_id', TEAMS_APP_ID);
   loginUrl.searchParams.append('redirect_uri', 'https://teams.microsoft.com/go');
-  loginUrl.searchParams.append('state', uuid.v4());
   loginUrl.searchParams.append('x-client-SKU', 'Js');
   loginUrl.searchParams.append('x-client-Ver', '1.0.9');
-  loginUrl.searchParams.append('nonce', uuid.v4());
+  loginUrl.searchParams.append('nonce', uuidv4());
 
   return loginUrl.toString();
 }
 
+function authorize(type: TeamsSkype) {
+  console.log(`Authorizing ${type}`);
+  win.loadURL(getLoginURL(type), {
+    userAgent: USER_AGENT,
+  });
+}
+
+function saveTeamsToken(token: string, type: TeamsSkype) {
+  if (!existsSync(`${homedir}/${configPath}/`)) {
+    mkdirSync(`${homedir}/${configPath}`, { recursive: true });
+  }
+  writeFileSync(`${homedir}/${configPath}/token-${type}.jwt`, token);
+}
+
 app.whenReady().then(() => {
-  const win = new BrowserWindow({ width: 800, height: 600 });
+  win = new BrowserWindow({ width: 800, height: 600 });
+  if (DEBUG) {
+    win.webContents.openDevTools();
+  }
   win.webContents.on('will-navigate', (e, url) => {
     if (url.startsWith('https://teams.microsoft.com/')) {
       e.preventDefault();
@@ -30,16 +71,53 @@ app.whenReady().then(() => {
 
   win.webContents.on('did-navigate', (e, url) => {
     if (url.startsWith('https://teams.microsoft.com/go')) {
-      e.preventDefault();
       const token = url.replace('https://teams.microsoft.com/go#', '');
       const searchParams = new URLSearchParams(token);
-      console.log(searchParams.get('id_token'));
-      win.destroy();
-      app.quit();
+      let teamsToken = searchParams.get('id_token');
+
+      if (teamsToken === null) {
+        teamsToken = searchParams.get('access_token');
+      }
+
+      const decoded = jwt.decode(teamsToken);
+      if (decoded === null) {
+        console.warn(`Inavlid JWT provided: ${searchParams}`);
+        return;
+      }
+
+      if (typeof (decoded) === 'string') {
+        console.error('Invalid decoded JWT: is a string');
+        return;
+      }
+
+      tokenResponseCount += 1;
+
+      if (tokenResponseCount > 3) {
+        console.error('Redirecting too many times, stopping');
+        e.preventDefault();
+        win.webContents.stop();
+        return;
+      }
+
+      if (decoded.aud === TEAMS_APP_ID) {
+        // Teams Token
+        console.log('Got a Teams token');
+        e.preventDefault();
+        win.webContents.stop();
+
+        saveTeamsToken(teamsToken, 'teams');
+        // Go To SKYPE_RESOURCE authorization
+        authorize('skype');
+      } else if (decoded.aud === SKYPE_RESOURCE) {
+        console.log('Got a Skype token');
+        saveTeamsToken(teamsToken, 'skype');
+        win.destroy();
+        app.quit();
+      } else {
+        console.error(`Invalid audience ${decoded.aud} found.`);
+      }
     }
   });
 
-  win.loadURL(getLoginURL(), {
-    userAgent: USER_AGENT,
-  });
+  authorize('teams');
 });
